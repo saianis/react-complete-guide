f575ac4051a3e809c686d383f3dbaf241cc22bc0
saianis <anish@Anishs-MacBook-Pro.local>
2020-05-04 22:23:47 +0530

    used useContext hook

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -11,17 +11,19 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
+    
   }
 
   render() {
   console.log('[Person.js] r=rendering....');
   return (
     <Aux>
-      <AuthContext.Consumer>
-        {(context) => context.authenticated ? <p>Authenticated!!!</p> : <p>Please Log in!!</p>}
-      </AuthContext.Consumer>
+      {this.context.authenticated ? <p>Authenticated!!!</p> : <p>Please Log in!!</p>}
       <p key="2"onClick={this.props.click}>
         I'm {this.props.name} and I am {this.props.age} years old!
       </p>
