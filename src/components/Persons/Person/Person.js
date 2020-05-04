393f43be1c638260aa6a3f42d8d4466b7aa0f054
saianis <anish@Anishs-MacBook-Pro.local>
2020-05-04 20:17:51 +0530

    introduced higher order components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,19 @@
 import React, { Component }from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
   render() {
   console.log('[Person.js] r=rendering....');
   return (
-    <div className={classes.Person}>
-      <p onClick={this.props.click}>
+    <Aux>
+      <p key="2"onClick={this.props.click}>
         I'm {this.props.name} and I am {this.props.age} years old!
-      </p>
-      <p>{this.props.children}</p>
-      <input type="text" onChange={this.props.changed} value={this.props.name} />
-    </div>
-  );
+      </p>,
+      <p key="i2">{this.props.children}</p>,
+      <input key="key3" type="text" onChange={this.props.changed} value={this.props.name} />
+    </Aux>
+  )
   }
 };
 
