14c46980e174c644a7fad6848f21a0ec54529733
saianis <anish@Anishs-MacBook-Pro.local>
2020-05-04 21:14:58 +0530

    introduced proptypes into persons component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component }from 'react';
+import PropTypes from 'prop-types';
 import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
@@ -16,6 +17,13 @@ class Person extends Component {
     </Aux>
   )
   }
+};
+
+Person.PropTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
 };
 
 export default withClass(Person, classes.Person);
