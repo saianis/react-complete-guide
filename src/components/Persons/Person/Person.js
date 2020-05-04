2e0649b8e119266d7ca35ffbcf154283cac7a676
saianis <anish@Anishs-MacBook-Pro.local>
2020-05-04 21:07:33 +0530

    setting stae correctly

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component }from 'react';
 import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
   render() {
@@ -17,4 +18,4 @@ class Person extends Component {
   }
 };
 
-export default Person;
+export default withClass(Person, classes.Person);
